fe35d7dd69f403ede053416fb75af1b0aca160b5
Ion-Alexandru Rosu <alexandru.rosu@capgemini.com>
2023-07-24 12:40:10 +0300

    ---

---
+++ src/main/Deck.java
@@ -0,0 +1,176 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Deck {
+
+    public static int[] removeCard(int[] arr, int index) {
+        return IntStream.range(0, arr.length)
+                .filter(i -> i != index)
+                .map(i -> arr[i])
+                .toArray();
+    }
+//adauga arrays fill in main sau cv
+static int[] ofSpades= new int[13];
+    static int[] ofHearts= new int[13];
+    static int[] ofDiamonds= new int[13];
+    static int[] ofClubs= new int[13];
+
+
+    public static void fillDeck(int[] array) {
+
+
+        for (int i=0;i<array.length;i++) {
+            Random random = new Random();
+            int number = random.nextInt(1,13);
+            array[i]=number;
+        }
+
+    }
+    public static int takeACard(){
+        System.out.println(Arrays.toString(ofClubs));
+         int card=0;
+       Random random = new Random();
+        Random random2 = new Random();
+
+       int deck = random2.nextInt(4);
+
+       switch (deck){
+           case 0 -> { int pos=random.nextInt(ofSpades.length);
+               if(pos<=10 && pos!=0){
+                   card = ofSpades[pos];
+                   System.out.println(card + " of Spades");
+               } else if(pos==0){card = ofSpades[pos];
+                   System.out.println("Ace of Spades");} else {card = 10;
+                   switch (pos) {
+                       case 10 -> System.out.println("J of Spades");
+                       case 11 -> System.out.println("Q of Spades");
+                       case 12 -> System.out.println("K of Spades");
+                   }
+               }
+
+
+               removeCard(ofSpades, pos);
+           }
+           case 1 -> { int pos=random.nextInt(ofHearts.length);
+               if(pos<=9 && pos!=0){
+                   card = ofHearts[pos];
+                   System.out.println(card + " of Hearts");
+               } else if(pos==0){card = ofHearts[pos];
+                   System.out.println("Ace of Hearts");} else {card = 10;
+                   switch (pos) {
+                       case 10 -> System.out.println("J of Hearts");
+                       case 11 -> System.out.println("Q of Hearts");
+                       case 12 -> System.out.println("K of Hearts");
+                   }
+               }
+
+
+               removeCard(ofHearts, pos);
+           }
+           case 2 -> { int pos=random.nextInt(ofClubs.length);
+               if(pos<=9 && pos!=0){
+                   card = ofClubs[pos];
+                   System.out.println(card + " of Clubs");
+               } else if(pos==0){card = ofSpades[pos];
+                   System.out.println("Ace of Clubs");} else {card = 10;
+                   switch (pos) {
+                       case 10 -> System.out.println("J of Clubs");
+                       case 11 -> System.out.println("Q of Clubs");
+                       case 12 -> System.out.println("K of Clubs");
+                   }
+               }
+
+
+               removeCard(ofClubs, pos);
+           }
+           case 3 -> { int pos=random.nextInt(ofDiamonds.length);
+               if(pos<=9 && pos!=0){
+                   card = ofDiamonds[pos];
+                   System.out.println(card + " of Diamonds");
+               } else if(pos==0){card = ofDiamonds[pos];
+                   System.out.println("Ace of Diamonds");} else {card = 10;
+                   switch (pos) {
+                       case 10 -> System.out.println("J of Diamonds");
+                       case 11 -> System.out.println("Q of Diamonds");
+                       case 12 -> System.out.println("K of Diamonds");
+                   }
+               }
+
+
+               removeCard(ofClubs, pos);
+           }
+
+
+
+       }
+
+
+        return card;
+
+    }
+    boolean win;
+    static int balance = 100;
+
+   static void imparteCarti(){
+        int pariu=0;
+        String hit ="";
+        int dHand = takeACard();
+        int pHand = takeACard() + takeACard();
+        System.out.println("Balance " + balance + "$");
+        System.out.println("Place your bet: ");
+        Scanner scanner2 = new Scanner(System.in);
+        pariu = scanner2.nextInt();
+        if (pHand==21) {
+            System.out.println("Blackjack!"); balance=balance+pariu;
+        } else if (pHand!=21) { while(true) {
+            System.out.println("Hit or stand?");
+            Scanner scanner = new Scanner(System.in);
+            hit = scanner.nextLine();
+            if (hit.equalsIgnoreCase("hit")){ pHand= pHand+takeACard(); if (pHand>21) {
+                System.out.println(pHand);
+                System.out.println("Bust!!!");
+                balance = balance - pariu;
+             break;}
+
+
+
+            } else{break;}
+        }
+
+        }
+
+        if (pHand<21){
+        System.out.println("Your hand : " +pHand);
+        dHand = dHand + takeACard();
+        while(dHand<17) { dHand = dHand + takeACard();}
+
+        if (dHand>=17 || dHand<=21){ if(dHand>pHand) {
+            System.out.println("You lose!"); balance = balance - pariu;
+        } else if (dHand==pHand) {
+            System.out.println("Tie");
+
+        } else {
+            System.out.println("You win!");
+            balance = balance + pariu;
+        }}
+
+        } else if (dHand>21){
+            System.out.println("Dealer bust! You win ! ");
+            balance = balance + pariu;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+    fillDeck (ofClubs);
+    fillDeck (ofSpades);
+    fillDeck (ofDiamonds);
+    fillDeck (ofHearts);
+    imparteCarti();
+    }
+}
---
+++ src/main/Rockpaparescissors.java
@@ -0,0 +1,102 @@
+package main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Rockpaparescissors {
+
+
+    public static void main(String[] args) {
+        String ans = "";
+        String userInput;
+        int balance = 100;
+        Scanner scan = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+        Random random = new Random();
+        while (true) {
+
+            int bet = 0;
+
+            if (bet == 0) {
+                System.out.println("Your balance:" + balance + "$");
+                System.out.println("How much will you bet?");
+            }
+
+            bet = scan.nextInt();
+
+
+            if (bet > balance) {
+                System.out.println("Bet exceeds balance");
+                break;
+            }
+
+            System.out.println("Place your bet: Rock, Paper, or Scissors?");
+            userInput = scan2.nextLine();
+
+//
+//       double rand = Math.random()*10/4;
+//       int rand2=(int)rand;
+            int rand2 = random.nextInt(3);
+
+            //   System.out.println(rand2);
+
+
+            switch (rand2) {
+                default -> System.out.println("error");
+                case 0 -> ans = "Rock";
+                case 1 -> ans = "Paper";
+                case 2 -> ans = "Scissors";
+            }
+            System.out.println("Computer chose: " + ans);
+
+            if (userInput.equalsIgnoreCase("Rock")) {
+                switch (ans) {
+                    case "Rock" -> System.out.println("Tie");
+                    case "Paper" -> {
+                        System.out.println("You lose");
+                        balance = balance - bet;
+                    }
+                    case "Scissors" -> {
+                        System.out.println("You win");
+                        balance = balance + bet;
+                    }
+                }
+            } else if (userInput.equalsIgnoreCase("Paper")) {
+                switch (ans) {
+                    case "Rock" -> {
+                        System.out.println("You win");
+                        balance = balance + bet;
+                    }
+                    case "Paper" -> System.out.println("Tie");
+                    case "Scissors" -> {
+                        System.out.println("You lose");
+                        balance = balance - bet;
+                    }
+                }
+            } else if (userInput.equalsIgnoreCase("Scissors")) {
+                switch (ans) {
+                    case "Rock" -> {
+                        System.out.println("You win");
+                        balance = balance + bet;
+                    }
+                    case "Paper" -> {
+                        System.out.println("You lose");
+                        balance = balance - bet;
+                    }
+                    case "Scissors" -> System.out.println("Tie");
+                }
+            } else {
+                System.out.println("Not a valid bet");
+            }
+
+
+            if (balance <= 0) {
+                System.out.println("You're bankrupt !!!");
+                break;
+            }
+
+        }
+
+    }
+
+}
